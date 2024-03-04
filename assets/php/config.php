2169e553d6dc558273fd276158d69a76cdf37ca0
ojiputra <ojishefals2@gmail.com>
2024-03-04 12:10:57 +0700

    ubah database

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,5 +1,5 @@
 <?php 
-$conn = mysqli_connect("localhost","root","","u1579055_db_rstonggakhusada");
+$conn = mysqli_connect("localhost","u826981306_rstgh","","u826981306_db_rstonggak");
  
 // Check connection
 if (mysqli_connect_errno()){
